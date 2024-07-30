01beee87dec9ce8d01f3addc85bae876d348a49f
Nyuan Zhang <blueglassblock@outlook.com>
2024-07-30 04:48:08 +0000

    update: moe icp

---
+++ components/YunFooter.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import YunFooter from 'valaxy-theme-yun/components/YunFooter.vue'
+</script>
+
+<template>
+  <YunFooter>
+    <a href="https://icp.gov.moe/?keyword=20248596" target="_blank">萌ICP备20248596号</a>
+  </YunFooter>
+</template>
